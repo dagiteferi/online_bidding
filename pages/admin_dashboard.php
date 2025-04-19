de1c3c7f1766f758775c4dca5359a9ed471d817b
Dagiteferi <dagiteferi2011@gmail.com>
2025-04-19 11:51:05 -0400

    Refactor admin dashboard styles; remove unused CSS stylesheet link for improved maintainability

--- pages/admin_dashboard.php
+++ pages/admin_dashboard.php
@@ -906,7 +906,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     <link rel="stylesheet" href="../css/admin.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <!-- <link rel="stylesheet" href="../css/style.css"> -->
     <script src="../javaScript/scripts.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
